24492d98386af531759a1b319096a3592dda643f
Akif <mdakifchowdhury20@gmail.com>
2021-01-17 02:49:47 +0600

    Added the cases command

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 import os
 import discord
+from covid import Covid
 from discord.ext import commands
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DISCORD_KEY = os.getenv('DISCORD_KEY')
 
 discord_client = commands.Bot(command_prefix='!')
+covid = Covid()
 
 @discord_client.event
 async def on_ready():
@@ -19,5 +18,18 @@ async def commands(ctx):
    + "!rank (country): displays the rank of selected country based on number of cases \n" 
    + "!rate (country): displays the rate of change of cases of selected country")
 
+@discord_client.command()
+async def cases(ctx, *args):
+   if args:
+      country = args[0]
+      cas = covid.get_status_by_country_name(f"{country}")
+      await ctx.channel.send(f"Cases in {country} - {int(cas['active']):,}")
+   
+   else:
+      await ctx.channel.send(f"Give a country name")
+   
+@discord_client.command()
+async def rank(ctx, *args):
+   pass
 
 discord_client.run(DISCORD_KEY)

612322c3de45fbadb856716bded220d1ba3db04e
Jack Parsons <jackpar1113@gmail.com>
2021-01-16 13:53:25 -0700

    update !commands

--- bot.py
+++ bot.py
@@ -14,7 +14,8 @@ async def on_ready():
 async def commands(ctx):
    await ctx.channel.send("!cases (country): displays the number of cases of selected country\n" 
    + "!rank (country): displays the rank of selected country based on number of cases \n" 
-   + "!rate (country): displays the rate of change of cases of selected country")
+   + "!rate (country): displays the rate of change of cases of selected country\n"
+   + "!deaths (country) displays the number of deaths of selected country" )
 
 
 discord_client.run(DISCORD_KEY)
